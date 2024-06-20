0594c55d58b350655a8b080eefccb0be6c9340ca
tarun <tarunrk2002@gmail.com>
2024-06-20 10:55:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_Ienumerable
+{
+    internal class Program
+    {
+
+        public void hi() 
+        {
+
+        }
+        static void Main(string[] args)
+        {
+           
+            
+        }
+    }
+}
